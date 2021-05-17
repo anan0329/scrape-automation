02a5d89def678a5f9bb005aa5aa35fd74cff22af
Brian Yang <anan0329@yahoo.com.tw>
2021-05-17 13:39:46 +0800

    Update scraping.R

--- scraping.R
+++ scraping.R
@@ -1,56 +1,24 @@
-#load libs
-
 library(tidyverse)
 library(rvest)
 library(janitor)
 
-#nse top gainers
 
-url <- 'https://www.moneycontrol.com/stocks/marketstats/nsegainer/index.php'
+url_base <- "https://www.twse.com.tw/exchangeReport/MI_INDEX20?response=html&date="
+url <- url_base %>% 
+    paste0(
+        format(Sys.Date(), "%Y%m%d")
+    )
 
-# extract html 
 
-url_html <- read_html(url)
+url_html <- read_html(url, encoding = "UTF-8")
+url_html %>% 
+    html_elements("thead td") %>% 
+    html_text() -> table_title
+url_html %>% 
+    html_elements("tbody td") %>% 
+    html_text() %>% 
+    matrix(ncol=13, byrow=T) -> top_volumn
+colnames(top_volumn) <- table_title
 
-#table extraction
 
-url_tables <- url_html %>% html_table(fill = TRUE)
-
-#extract relevant table
-
-top_gainers <- url_tables[[2]]
-
-#extract relevant columns
-
-top_gainers %>%
-  select(1:7) -> top_gainers
-
-top_gainers %>% 
-  clean_names() -> top_gainers
-
-top_gainers %>%
-  filter(!is.na(low)) -> top_gainers
-
-top_gainers %>%
-  separate(company_name,
-           into = 'company_name',
-           sep = '\t') -> top_gainers
-           
-           
- write_csv(top_gainers,paste0('data/',Sys.Date(),'_top_gainers','.csv'))    
- 
-# Top Gainers Chart 
-
-# top_gainers %>% 
-#   arrange(-percent_gain) %>%
-#   head(10) %>%
-#   mutate(company_name = fct_reorder(company_name,percent_gain)) %>%
-#   ggplot() + 
-#   geom_col(aes(y = company_name,
-#                       x = percent_gain)) +
-#   geom_label(aes(y = company_name,
-#                  x = percent_gain,
-#                  label = last_price)) +
-#   theme_minimal()+
-#   labs(title = "Top Gainer - Nifty50",
-#        subtitle = 'using R')
+if(!all(is.na(top_volumn))) write_csv(top_volumn, paste0('data/',Sys.Date(),'_top_volumn','.csv'))
